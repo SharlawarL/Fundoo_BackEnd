13b6a482ad02e3bd469a84def014ee4131f36e93
SharlawarL <sharlawar77@gmail.com>
2019-09-04 10:40:41 +0530

    Removed header access control from user model

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,9 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, X-Auth-Token, X-PINGOTHER, Content-Type');
-header('Access-Control-Max-Age: 86400');
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model {
 
