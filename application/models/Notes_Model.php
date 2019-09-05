f62b48b6ffd4a8378d39a8013142daa6dd723a72
SharlawarL <sharlawar77@gmail.com>
2019-09-05 15:10:39 +0530

    Updated code and added some commits

--- application/models/Notes_Model.php
+++ application/models/Notes_Model.php
@@ -2,16 +2,14 @@
 
 class Notes_Model extends CI_Model {
 
+    //for inserting data
     function insertNote($NotesData)
     {
-        //inserting Notes into database
         $result = $this->db->insert('Notes',$NotesData);
     }
 
     function get_Notes(){
-        // retriving notes
         $Notes = $this->db->get('Notes');
-        // returning in the formate of array
         return $Notes->result_array();
     }
 
