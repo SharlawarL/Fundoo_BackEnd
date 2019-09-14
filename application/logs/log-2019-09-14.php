c3e2f25859d9dadd5397ffdb9277f623ab39b66e
SharlawarL <sharlawar77@gmail.com>
2019-09-14 12:32:10 +0530

    Phpunit will be installed

---
+++ application/logs/log-2019-09-14.php
@@ -0,0 +1,42 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2019-09-14 06:06:10 --> Config Class Initialized
+INFO - 2019-09-14 06:06:10 --> Hooks Class Initialized
+DEBUG - 2019-09-14 06:06:10 --> UTF-8 Support Enabled
+INFO - 2019-09-14 06:06:10 --> Utf8 Class Initialized
+INFO - 2019-09-14 06:06:10 --> URI Class Initialized
+INFO - 2019-09-14 06:06:11 --> Router Class Initialized
+INFO - 2019-09-14 06:06:11 --> Output Class Initialized
+INFO - 2019-09-14 06:06:11 --> Security Class Initialized
+DEBUG - 2019-09-14 06:06:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-14 06:06:11 --> Input Class Initialized
+INFO - 2019-09-14 06:06:11 --> Language Class Initialized
+INFO - 2019-09-14 06:06:11 --> Loader Class Initialized
+INFO - 2019-09-14 06:06:11 --> Controller Class Initialized
+INFO - 2019-09-14 06:06:11 --> Loader Class Initialized
+INFO - 2019-09-14 06:06:45 --> Config Class Initialized
+INFO - 2019-09-14 06:06:45 --> Hooks Class Initialized
+DEBUG - 2019-09-14 06:06:45 --> UTF-8 Support Enabled
+INFO - 2019-09-14 06:06:45 --> Utf8 Class Initialized
+INFO - 2019-09-14 06:06:45 --> URI Class Initialized
+INFO - 2019-09-14 06:06:45 --> Router Class Initialized
+INFO - 2019-09-14 06:06:45 --> Output Class Initialized
+INFO - 2019-09-14 06:06:45 --> Security Class Initialized
+DEBUG - 2019-09-14 06:06:45 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-14 06:06:45 --> Input Class Initialized
+INFO - 2019-09-14 06:06:45 --> Language Class Initialized
+INFO - 2019-09-14 06:06:45 --> Loader Class Initialized
+INFO - 2019-09-14 06:06:45 --> Controller Class Initialized
+INFO - 2019-09-14 06:06:45 --> Loader Class Initialized
+INFO - 2019-09-14 06:06:45 --> Config Class Initialized
+INFO - 2019-09-14 06:06:45 --> Hooks Class Initialized
+INFO - 2019-09-14 06:06:45 --> URI Class Initialized
+DEBUG - 2019-09-14 06:06:45 --> No URI present. Default controller set.
+INFO - 2019-09-14 06:06:45 --> Router Class Initialized
+INFO - 2019-09-14 06:06:45 --> Output Class Initialized
+INFO - 2019-09-14 06:06:45 --> Security Class Initialized
+DEBUG - 2019-09-14 06:06:45 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-14 06:06:45 --> Input Class Initialized
+INFO - 2019-09-14 06:06:45 --> Language Class Initialized
+INFO - 2019-09-14 06:06:45 --> Loader Class Initialized
+INFO - 2019-09-14 06:06:45 --> Controller Class Initialized
