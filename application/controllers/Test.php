764f97bf76605d5472570ea632909c97680f0435
SharlawarL <sharlawar77@gmail.com>
2019-09-17 17:56:27 +0530

    test case for the database API check

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,25 +1,159 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use CodeIgniter\Test\CIDatabaseTestCase;
+
 class Test extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
         $this->load->library('unit_test');
+        $this->load->database();
+        $this->load->model('User_model');
 	}
 
+    // for Display testing test case are working
 	public function index()
 	{
-        echo "Welcome to test case \n \n \n ";
-        $test = 1 + 1;
+        //check for database connection
+        $this->Db_test();
 
-        $expected_result = 2;
+        //check for user login
+        $this->Test_user_login();
 
-        $test_name = 'Adds one plus one';
+        //check for register User
+        $this->Test_user_register();
 
-        echo $this->unit->run($test, $expected_result, $test_name);
+        //verifing the mail
+        $this->Test_email_verify();
 
-        echo $this->unit->run('Foo', 'Foo');
+        // for reseting password
+        $this->Test_reset_password();
     }
-    
+
+    //test case for database connection
+    public function Db_test()
+    {
+        $test_result = false;
+        $title = "Database Connection Testing";
+        echo $title;
+        /** 
+         *   Load the database config file.
+         */
+        if(file_exists($file_path = APPPATH.'config/database.php'))
+        {
+            include($file_path);
+        }
+        $db = $db[$active_group];
+        /** 
+         *   Check database connection if using mysqli driver
+         */
+        if( $db['dbdriver'] === 'mysqli' )
+        {
+            $mysqli = new mysqli( $db['hostname'] , $db['username'] , $db['password'], $db['database']);
+            if(!$mysqli->connect_error )
+            {
+                $test_result= true;
+            }
+        }
+        /** 
+         *   Expected Result
+         */
+        $Expected_result = true;
+        echo $this->unit->run($test_result,$Expected_result,$title);
+    }
+
+    //test case for the retiving User Data
+    function Test_user_login()
+    {
+        $User_Test_title = "Login User Data Test";
+
+        echo $User_Test_title;
+
+        /**
+         * In the result generation. I have pass one one Email Id For the user Login.
+         * which is check into my User_model and return the user data
+         */
+        $data['email'] = "click@gmail.com";
+        $User_data = $this->User_model->login_user('user',$data);
+
+        /**
+         * Expected result
+         */
+        $Expetected_data = "Shubham";
+
+        echo $this->unit->run($User_data->firstname,$Expetected_data,$User_Test_title);  
+    }
+
+    //test case for the retiving User Data
+    function Test_user_register()
+    {
+        // title for test case
+        $User_Test_title = "User Registration Data Test";
+        echo $User_Test_title;
+
+        /**
+         * Sample data can be inserted into data base for test
+         */
+        $data['firstname'] = "lalit";
+        $data['lastname'] = "sharlawar";
+        $data['email'] = "sharlawar@gmail.com";
+        $data['password'] = "123";
+
+        $result = $this->User_model->insert_user('user',$data);
+
+        /**
+         * Expected result generation
+         */
+        $Expetected_result = true;
+
+        echo $this->unit->run($result,$Expetected_result,$User_Test_title);  
+    }
+
+    //test for Email verification
+    function Test_email_verify()
+    {
+        // title for test case
+        $User_Test_title = "User Email verification Test";
+        echo $User_Test_title;
+
+        /**
+         * The user has been verified there mail
+         * let's consider user id 1. this your want to verify mail
+         */
+        $User_id = '166';
+
+        $result = $this->User_model->update_mail_status('user',$User_id);
+
+        /**
+         * Expected result generation
+         */
+        $Expetected_result = true;
+
+        echo $this->unit->run($result,$Expetected_result,$User_Test_title);
+    }
+
+    // test for reset password
+    function Test_reset_password()
+    {
+        $User_Test_title = "Test Case Reset password";
+        echo $User_Test_title;
+        /** 
+         *   Parameter pass for reset password
+         */
+        $data['id']="166";
+        $new_password = "12345";
+
+        $result = $this->User_model->reset_password('user',$data,$new_password);
+
+        /**
+         * Expected result generation
+         */
+        $Expetected_result = true;
+
+        echo $this->unit->run($result,$Expetected_result,$User_Test_title);
+
+    }
 }
+
+
