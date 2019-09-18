6c63f5599aa18430396296db09c7bdf9a59ac8ce
SharlawarL <sharlawar77@gmail.com>
2019-09-18 16:31:02 +0530

    The database api for checking PHPunit

---
+++ application/tests/models/User_model.php
@@ -0,0 +1,66 @@
+<?php
+class User_model extends CI_Model {
+
+    //check for the login user
+    function login_user($table_name,$User_data){
+
+        $query =  $this->db->get_where($table_name,$User_data);
+        foreach ($query->result() as $row)
+        {
+            return $row;
+        }
+    }
+
+    //inserting User into the table
+    function insert_user($table_name,$data){
+        $result = $this->db->insert($table_name,$data);
+        return $result;
+    }
+
+    //verify mail
+    function update_mail_status($table_name,$User_id){
+        $this->db->set('email_verify', '1');
+        $this->db->where('id', $User_id);
+        $result = $this->db->update($table_name); 
+        return $result;
+    }
+
+    //getting perticular user ID
+    public function get_ID($table_name,$data)
+    {
+        $query =  $this->db->get_where($table_name,$data);
+
+        foreach ($query->result() as $row)
+        {
+            return $row->id;
+        }
+    }
+
+    //check user
+    public function check_user($table_name,$User_data)
+    {
+        $email = $User_data['email'];
+        $query = $this->db->query("select * from ".$table_name." where `email`='".$email."'");
+        
+        foreach ($query->result() as $row)
+        {
+            return $row->id;
+        }
+    }
+
+    //for reset password
+    public function reset_password($table_name,$data,$new_password){
+        $this->db->set('password', $new_password);
+        $this->db->where($data);
+        return $this->db->update($table_name);
+    }
+
+    // get user details
+    function get_user_details($id){
+        $this->db->where('id',$id);
+        $Notes = $this->db->get('user');
+        return $Notes->result_array();
+    }
+
+
+}
